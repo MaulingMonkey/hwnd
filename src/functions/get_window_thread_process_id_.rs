7b035f44b941a5e51854bf73358635cc90603ec4
MaulingMonkey <git@maulingmonkey.com>
2022-02-15 12:35:50 -0800

    get_window_process_id

--- src/functions/get_window_thread_process_id_.rs
+++ src/functions/get_window_thread_process_id_.rs
@@ -32,11 +32,50 @@ use winapi::um::winuser::*;
 ///
 /// ### See Also
 /// *   [get_window_thread_process_id]
+/// *   [get_window_process_id]
 #[must_use] pub fn get_window_thread_id(hwnd: impl Into<HWND>) -> Result<u32, Error> {
     fn_context!(get_window_thread_id => GetWindowThreadProcessId);
     let hwnd = hwnd.into();
     let tid = unsafe { GetWindowThreadProcessId(hwnd, std::ptr::null_mut()) };
     if tid != 0 { Ok(tid) } else { Err(fn_error_gle!()) }
+}
+
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowthreadprocessid)\]
+/// GetWindowThreadProcessId
+///
+/// Retrieves the identifier of the thread that created the specified window.
+///
+/// ### Returns
+/// *   Ok(thread_id)
+/// *   [ERROR::INVALID_WINDOW_HANDLE]
+///
+/// ### Example
+/// ```rust
+/// # use hwnd::*;
+/// # use winerr::*;
+/// # use std::ptr::*;
+/// # let hwnd = get_desktop_window();
+/// let process = get_window_process_id(hwnd).unwrap();
+/// let hwnd_belongs_to_this_process = process == get_current_process_id();
+/// # assert!(!hwnd_belongs_to_this_process, "desktop doesn't belong to us!");
+/// #
+/// # assert_eq!(ERROR::INVALID_WINDOW_HANDLE, get_window_process_id(null_mut()).unwrap_err());
+/// # for p in 0 .. 8 * std::mem::size_of::<HWND>() {
+/// #   if let Err(e) = get_window_process_id((1usize << p) as HWND) { // shouldn't crash
+/// #       assert_eq!(ERROR::INVALID_WINDOW_HANDLE, e);
+/// #   }
+/// # }
+/// ```
+///
+/// ### See Also
+/// *   [get_window_thread_id]
+/// *   [get_window_thread_process_id]
+#[must_use] pub fn get_window_process_id(hwnd: impl Into<HWND>) -> Result<u32, Error> {
+    fn_context!(get_window_process_id => GetWindowThreadProcessId);
+    let hwnd = hwnd.into();
+    let mut pid = 0;
+    let tid = unsafe { GetWindowThreadProcessId(hwnd, &mut pid) };
+    if tid != 0 { Ok(pid) } else { Err(fn_error_gle!()) }
 }
 
 /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowthreadprocessid)\]
@@ -67,6 +106,7 @@ use winapi::um::winuser::*;
 /// ```
 ///
 /// ### See Also
+/// *   [get_window_process_id]
 /// *   [get_window_thread_id]
 #[must_use] pub fn get_window_thread_process_id(hwnd: impl Into<HWND>) -> Result<(u32, u32), Error> {
     fn_context!(get_window_thread_process_id => GetWindowThreadProcessId);
@@ -75,3 +115,5 @@ use winapi::um::winuser::*;
     let tid = unsafe { GetWindowThreadProcessId(hwnd, &mut pid) };
     if tid != 0 { Ok((tid, pid)) } else { Err(fn_error_gle!()) }
 }
+
+// TODO: test coverage with process/thread local windows
