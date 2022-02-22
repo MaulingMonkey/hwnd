ce84ebc4a1cbbc0e583749fe35bd50044c841ec0
MaulingMonkey <git@maulingmonkey.com>
2022-02-21 23:15:45 -0800

    test/doc --all-features

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,9 +1,11 @@
 @setlocal && pushd "%~dp0.."
 cargo run --manifest-path=crates/xtask/Cargo.toml --bin gen
 @if ERRORLEVEL 1 goto :end
+cargo test --all-features
+@if ERRORLEVEL 1 goto :end
 cargo test
 @if ERRORLEVEL 1 goto :end
-cargo doc
+cargo doc --all-features
 @if ERRORLEVEL 1 goto :end
 :end
 @endlocal && popd && exit /b %ERRORLEVEL%
--- scripts/help.cmd
+++ scripts/help.cmd
@@ -1,7 +1,7 @@
 @setlocal && pushd "%~dp0.."
 cargo run --manifest-path=crates/xtask/Cargo.toml --bin gen
 @if ERRORLEVEL 1 goto :end
-cargo doc --open
+cargo doc --all-features --open
 @if ERRORLEVEL 1 goto :end
 :end
 @endlocal && popd && exit /b %ERRORLEVEL%
