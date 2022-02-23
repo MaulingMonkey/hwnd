ed00742e4c96d129d979cdc6720144dbacae54e9
MaulingMonkey <git@maulingmonkey.com>
2022-02-23 09:41:43 -0800

    scripts/build.cmd: running tests twice is overkill

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -3,9 +3,9 @@ cargo run --manifest-path=crates/xtask/Cargo.toml --bin gen
 @if ERRORLEVEL 1 goto :end
 cargo test --all-features
 @if ERRORLEVEL 1 goto :end
-cargo test
-@if ERRORLEVEL 1 goto :end
 cargo doc --all-features
+@if ERRORLEVEL 1 goto :end
+cargo build
 @if ERRORLEVEL 1 goto :end
 :end
 @endlocal && popd && exit /b %ERRORLEVEL%
